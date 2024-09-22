6545e3ce12693cc95647fc739b0a213be88c4252
Salar <salarkhanlala@gmail.com>
2024-09-22 19:27:58 +0500

    updated index.html, vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: () => 'everything.js',
-      },
-    },
-  },
-});
+  plugins: [react()],
+  base: "/homePage"
+})
